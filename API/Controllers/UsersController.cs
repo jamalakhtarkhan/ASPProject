7ffe6c8ae255cf6b012ab65ece3a6b7cbad9721b
jamalakhtarkhan <jamal.khan@comm-it.in>
2022-12-21 12:39:15 +0530

    End of section 4

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -1,18 +1,15 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using API.Data;
 using API.Entities;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
 {
-    [ApiController]
-    [Route("api/[controller]")]
 
-    public class UsersController : ControllerBase
+[Authorize]
+
+    public class UsersController : BaseApiControlers
     {
         
         private readonly DataContext _context;
@@ -22,6 +19,7 @@ namespace API.Controllers
             _context = context;
             
         }
+        [AllowAnonymous]
         [HttpGet]
         public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
         {
--- API/Program.cs
+++ API/Program.cs
@@ -1,38 +1,41 @@
+using System.Text;
 using API.Data;
+using API.Extensions;
+using API.Interface;
+using API.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+    // Add services to the container.
 
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-//builder.Services.AddEndpointsApiExplorer();
-//builder.Services.AddSwaggerGen();
-
-builder.Services.AddDbContext<DataContext>(opt=>{
-
-  opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
-
-});
-builder.Services.AddCors();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-app.UseCors(builder=>builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
-//if (app.Environment.IsDevelopment())
-//{
-  //  app.UseSwagger();
-   // app.UseSwaggerUI();
-//}
-
-//app.UseHttpsRedirection();
-
-//app.UseAuthorization();
+    builder.Services.AddControllers();
+    // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+    //builder.Services.AddEndpointsApiExplorer();
+    //builder.Services.AddSwaggerGen();
+    builder.Services.AddApplicationService(builder.Configuration);
+    builder.Services.AddIdentityServices(builder.Configuration);
+    var app = builder.Build();
 
 
 
-app.MapControllers();
+      // Configure the HTTP request pipeline.
+      app.UseCors(builder=>builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
+      //if (app.Environment.IsDevelopment())
+      //{
+        //  app.UseSwagger();
+        // app.UseSwaggerUI();
+      //}
 
-app.Run();
+      //app.UseHttpsRedirection();
+
+      //app.UseAuthorization();
+
+      app.UseAuthentication();
+      app.UseAuthorization();
+
+      app.MapControllers();
+
+      app.Run();
